abf7c25f84e27f2976261a0d3bcbfe837c5731d8
Letticia Nicoli <letticianicoli@unknown.home>
2019-08-06 18:48:28 -0300

    First commit

---
+++ TryDotNet_FirstSteps/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TryDotNet_FirstSteps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args[1])
+            {
+                case "FollowMeExample":
+                    FollowMeExample();
+                    break;
+                case "JsonExample":
+                    JsonExample();
+                    break;
+            }
+        }
+
+        static void FollowMeExample()
+        {
+            #region FollowMeExample
+            var twitter = "@LetticiaNicoli";
+            Console.WriteLine($"Follow me: {twitter}!");
+            #endregion FollowMeExample
+        }
+
+        static void JsonExample()
+        {
+            #region JsonExample
+            var times = new { text = "Now", time = DateTime.Now };
+            var json = JsonConvert.SerializeObject(times);
+
+            Console.WriteLine(json);
+            #endregion JsonExample
+        }
+    }
+}
---
+++ TryDotNet_FirstSteps/README.md
@@ -0,0 +1,15 @@
+# Primeiros passos com Try .NET
+
+**Execute o código abaixo e veja a mágica acontecer :)**
+
+### Imprimindo valor
+
+``` cs --region FollowMeExample --source-file .\Program.cs --project .\TryDotNet-FirstSteps.csproj 
+
+```
+
+### Utilizando pacotes
+
+``` cs --region JsonExample --source-file .\Program.cs --project .\TryDotNet-FirstSteps.csproj 
+
+```
